6a2dc09089abfd94927418cc9ce465d7b468c65e
Avinash Nishad <avinashn157@outlook.com>
2025-07-11 12:42:04 +0530

    added new migration that can add a contraint in column table
    static associate(models) {
          // define association here
    
          this.belongsTo(models.City , {
            foreignKey: 'cityId' ,
            onDelete: 'CASCADE' ,
            onUpdate: 'CASCADE'
    
          })
        }

--- src/models/airport.js
+++ src/models/airport.js
@@ -11,6 +11,13 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+
+      this.belongsTo(models.City , {
+        foreignKey: 'cityId' ,
+        onDelete: 'CASCADE' ,
+        onUpdate: 'CASCADE'
+
+      })
     }
   }
   Airport.init({
