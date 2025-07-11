1de8f67b1933500fa1767ffdd437ad831a2326a5
Avinash Nishad <avinashn157@outlook.com>
2025-07-11 12:21:33 +0530

    Added assoication of city and airport
    async up (queryInterface, Sequelize) {
        await queryInterface.addConstraint('Airports' , {
          type: 'foreign key' ,
          name: 'city_fkey_constraint',
          fields:['cityId'],
          references:{
            table:'Cities',
            field: 'id'
          },
          onUpdate: 'CASCADE',
          onDelete: 'CASCADE'
        })

---
+++ src/migrations/20250711062946-update-city-airport-association.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airports' , {
+      type: 'foreign key' ,
+      name: 'city_fkey_constraint',
+      fields:['cityId'],
+      references:{
+        table:'Cities',
+        field: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+
+    await queryInterface.removeConstraint('Airports' , 'city_fkey_constraint');
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
